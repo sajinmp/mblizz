e21e20b6e93344a62c9458d1ffa8342e70496a68
sajinmp <sajinprasadkm@gmail.com>
2014-04-22 00:36:33 +0530

    random question during signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,19 @@ class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
+  
+
+  def new
+
+    question = { 1 => "Which is the third alphabet in 'MBlizz'?", 
+                 2 => "Which is third planet from the sun?",
+                 3 => "How many alphabets are there in 'MBlizz'?",
+                 4 => "What is 3 + 4 ?" }
+    $random_number = rand(1..4)
+    @displayed = question[$random_number]
+    @user = User.new
+
+  end
 
   def index
 
@@ -10,12 +23,6 @@ class UsersController < ApplicationController
 
   end
 
-  def new
-  
-    @user = User.new
-
-  end
-  
   def show
     
     @user = User.find(params[:id])
@@ -24,14 +31,28 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+  
+    question = { 1 => "Which is the third alphabet in 'MBlizz'?",
+                 2 => "Which is third planet from the sun?",
+                 3 => "How many alphabets are there in 'MBlizz'?",
+                 4 => "What is 3 + 4 ?" }
+    @displayed = question[$random_number]
+    answer = { 1 => ['l', 'L'],
+               2 => ['earth', 'Earth', 'EARTH'],
+               3 => ['6', 'six', 'Six', 'SIX'],
+               4 => ['7', 'seven', 'Seven', 'SEVEN'] }
     @user = User.new(user_params)
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to MBlizz"
-      redirect_to @user
+    if answer[$random_number].include?(params[:random_q])
+      if @user.save
+        sign_in @user
+        flash[:success] = "Welcome to MBlizz"
+        redirect_to @user
+      else
+        render 'new'
+      end
     else
-      render 'new'
+      flash[:danger] = "Oops, Wrong answer to random question"
+      redirect_to signup_path
     end
 
   end
